101ba343d9f6c11305fb163e02dfa142e44d3728
ZohaibandSaeed <zohaibsaeed1701@gmail.com>
2026-09-08 16:12:33 +0500

    auth v1

--- Day15/server.js
+++ Day15/server.js
@@ -1,1 +1,19 @@
-console.log("HI");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log("Hello Word");
+    res.send("Hello Word");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
